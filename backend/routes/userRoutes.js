0bccb8c4e546e1012c2516c7e5376bf2fa3a3f99
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-12-23 21:17:39 +0200

    implement new routes for user profile and user register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const authUser = require("../controllers/userControllers");
+const {
+  authUser,
+  getUserProfile,
+  registerUser,
+} = require("../controllers/userControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
+router.route("/").post(registerUser);
 router.route("/login").post(authUser);
-
+router.route("/profile").get(protect, getUserProfile);
 module.exports = router;
